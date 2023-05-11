826c238346d4c6231f56031685ad4d0b3d3c81fa
dariorojas6 <dario43055@gmail.com>
2023-05-11 00:34:08 -0300

    enrutado

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from "react";
-import Header from "./componentes/Header/index.jsx";
-import  ProductosLista  from "./componentes/Productos/index.jsx";
+import React from 'react'
+import { Header } from "./componentes/Header";
+// import { Carrito } from "./componentes/Carrito";
+// import { DataProvider } from './componentes/context/DataProvider.js';
+import { BrowserRouter as Router } from "react-router-dom";
+import {Switch, Route} from "react-router-dom"
+// import Pages from "./componentes/Page.js";
+import Inicio from "./componentes/page/inicio/index.js"
 import "boxicons";
 
 function App() {
+
   return (
-    <div className="App">
-      <Header/>
-      <ProductosLista/>
-    </div>
+
+    // <DataProvider>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Inicio}/>
+          {/* <Route exact path="/productos" component={ProductosList}/> */}
+        </Switch>
+      </div>
+    </Router>
+    // </DataProvider >
   );
 }
 
